61f1fdc1501f0d171bdcf96d08ea33b78cbdfd4e
Tornike Jincharadze <tornikejincharadze1997@gmail.com>
2024-03-10 22:16:17 +0400

    feat: updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/vite-test/",
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode, isSsrBuild }) => {
+  console.log(command, mode, isSsrBuild);
+
+  if (command === 'serve') {
+    return {
+      // dev specific config
+      base: '/vite-test/',
+      plugins: [react()],
+    };
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+    };
+  }
+});
